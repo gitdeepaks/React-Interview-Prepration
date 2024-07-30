d3d29f767fc56dfc8a9f197988d80c9f8e2431c2
gitdeepaks <deepaksankhyan92@outlook.com>
2024-07-30 20:30:45 +0530

    pagination react

--- 22-pagination-react/src/App.jsx
+++ 22-pagination-react/src/App.jsx
@@ -8,9 +8,7 @@ const App = () => {
   const [totalPages, settotalPages] = useState(0);
 
   async function fetchProducts() {
-    const res = await axios.get(
-      `https://dummyjson.com/products?limit=10&skip=${page * 10 - 10}`
-    );
+    const res = await axios.get(`https://dummyjson.com/products?limit=100`);
     const data = await res.data;
     if (data && data.products) {
       setproducts(data.products);
@@ -38,41 +36,43 @@ const App = () => {
     <div>
       {products.length > 0 && (
         <div className="products">
-          {products.map((prd) => {
+          {products.slice(page * 10 - 10, page * 10).map((prod) => {
             return (
-              <span className="products__single" key={prd.id}>
-                <img src={prd.thumbnail} alt={prd.title} />
-                <span>{prd.title}</span>
+              <span className="products__single" key={prod.id}>
+                <img src={prod.thumbnail} alt={prod.title} />
+                <span>{prod.title}</span>
               </span>
             );
           })}
         </div>
       )}
+
       {products.length > 0 && (
         <div className="pagination">
           <span
-            onClick={() => page > 1 && selectPageHandler(page - 1)}
-            disabled={page <= 1}
+            onClick={() => selectPageHandler(page - 1)}
+            className={page > 1 ? "" : "pagination__disable"}
           >
-            ⏮️
+            ◀
           </span>
-          {[...Array(Math.ceil(products.length / 10))].map((_, index) => (
-            <span
-              className={page === index + 1 ? "pagination__selected" : ""}
-              onClick={() => selectPageHandler(index + 1)}
-              key={index}
-            >
-              {index + 1}
-            </span>
-          ))}
+
+          {[...Array(products.length / 10)].map((_, i) => {
+            return (
+              <span
+                key={i}
+                className={page === i + 1 ? "pagination__selected" : ""}
+                onClick={() => selectPageHandler(i + 1)}
+              >
+                {i + 1}
+              </span>
+            );
+          })}
+
           <span
-            onClick={() =>
-              page < Math.ceil(products.length / 10) &&
-              selectPageHandler(page + 1)
-            }
-            disabled={page >= Math.ceil(products.length / 10)}
+            onClick={() => selectPageHandler(page + 1)}
+            className={page < products.length / 10 ? "" : "pagination__disable"}
           >
-            ⏭️
+            ▶
           </span>
         </div>
       )}
