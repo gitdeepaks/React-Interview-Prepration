f2d318743a21f14b5aeec5a841a03424a51423bd
gitdeepaks <deepaksankhyan92@outlook.com>
2024-08-02 12:56:33 +0530

    added color boxes

---
+++ 24-color-boxes/script.js
@@ -0,0 +1,55 @@
+const boxConfig = [
+  {
+    color: "red",
+    width: "33.33%",
+  },
+  {
+    color: "green",
+    width: "33.33%",
+  },
+  {
+    color: "blue",
+    width: "33.33%",
+  },
+  {
+    color: "yellow",
+    width: "50%",
+  },
+  {
+    color: "orange",
+    width: "50%",
+  },
+  {
+    color: "purple",
+    width: "70%",
+  },
+  {
+    color: "pink",
+    width: "30%",
+  },
+];
+
+const container = document.createElement("div");
+
+container.classList = "container";
+
+boxConfig.forEach((config, index) => {
+  const box = document.createElement("div");
+  box.classList = "box";
+  box.style.backgroundColor = config.color;
+  box.style.width = config.width;
+
+  container.appendChild(box);
+});
+
+container.addEventListener("click", (e) => {
+  const clickElement = e.target;
+
+  if (clickElement.classList.contains("box")) {
+    const index = Array.from(container.children).indexOf(clickElement);
+    const config = boxConfig[index];
+    alert(`Box ${index + 1} is ${config.color} in color`);
+  }
+});
+
+document.body.appendChild(container);
