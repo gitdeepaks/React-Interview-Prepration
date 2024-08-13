8dce13bda82d3bdd1a2c2bba2af020ec384019ab
gitdeepaks <deepaksankhyan92@outlook.com>
2024-08-13 16:13:48 +0530

    useRef

--- 30-context-hook/src/App.jsx
+++ 30-context-hook/src/App.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { useState, useRef } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [name, setUserName] = useState("");
+  console.log("render", name);
 
-  return <h1>UseRef</h1>;
+  const textInput = useRef();
+  function onTextFocus() {
+    console.log("onTextFocus", textInput.current.value);
+    textInput.current.focus;
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        ref={textInput}
+        value={name}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button onClick={onTextFocus}>Focus</button>
+    </div>
+  );
 }
 
 export default App;
