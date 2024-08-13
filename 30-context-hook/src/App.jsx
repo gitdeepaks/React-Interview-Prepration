a1b9984fae8cab11c1d5c26a8613657d5f13bf2a
gitdeepaks <deepaksankhyan92@outlook.com>
2024-08-13 15:36:01 +0530

    useRef

--- 30-context-hook/src/App.jsx
+++ 30-context-hook/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1>UseContext</h1>;
+  return <h1>UseRef</h1>;
 }
 
 export default App;
