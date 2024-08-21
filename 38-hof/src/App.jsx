61db7328050cc19c11cd0e04d960e5c5d12eea67
gitdeepaks <deepaksankhyan92@outlook.com>
2024-08-21 17:47:46 +0530

    react input

---
+++ 38-hof/src/App.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function regiter(e) {
+    e.preventDefault();
+    console.log("Data", username, password, email);
+  }
+
+  return (
+    <>
+      <div className="App">
+        <form onSubmit={regiter}>
+          <label htmlFor="">Email</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label htmlFor="">UserName</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
+          <label htmlFor="">Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={() => {}}>Submit</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
